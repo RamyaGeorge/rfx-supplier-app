ab3d99ebbb499f21e71636c1f4be744a889415cb
Ramya <ramya@agilecybersolutions.com>
2026-04-17 16:10:33 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       lang="en"
       className={`${montserrat.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="h-full">{children}</body>
+      <body className="h-full" suppressHydrationWarning>{children}</body>
     </html>
   );
 }
